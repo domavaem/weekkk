840ff42f6aa7e799b70c9e8014ac8caa8adf0a3b
Booker <domavaem@gmail.com>
2022-02-07 18:09:53 +0900

    Modify: display week number

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const currentdate: any = new Date();
+  const oneJan: any = new Date(currentdate.getFullYear(), 0, 1);
+  const numberOfDays: number = Math.floor(
+    (currentdate - oneJan) / (24 * 60 * 60 * 1000)
   );
+  const result = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
+  console.log(
+    `The week number of the current date (${currentdate}) is ${result}.`
+  );
+
+  return <>{`The week number of the current date is ${result}.`}</>;
 }
 
 export default App;
